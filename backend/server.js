90a7eccd599aac50d8e8b3667d80e733712aefdb
Manik Dutt <manikdutt157@gmail.com>
2025-04-12 20:12:49 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"; // deploy
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,6 +10,7 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve(); // deploy
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -20,6 +22,13 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// deploy
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+// deploy
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 // app.get("/", (req, res) => {
   // root route https://localhost:5000/
